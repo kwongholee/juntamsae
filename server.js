a46b5eee81219f3f67e91041331bb199afb2934e
kwongholee <ghlee9883@gmail.com>
2023-09-09 17:50:04 +0900

    9.9 예상 기능까지 구현 완료

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const helmet = require('helmet');
 const MongoClient = require('mongodb').MongoClient;
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
@@ -11,17 +10,29 @@ const methodOverride = require('method-override');
 const express = require('express');
 const app = express();
 
-app.use(helmet());
-app.use('view engine', 'ejs');
 app.use(session({secret: process.env.SECRET_CODE, resave: true, saveUninitialized: false}));
 app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+var db;
+
+MongoClient.connect(process.env.DB_URL, (err, client) => {
+    if(err) return console.log(err);
+  
+    db = client.db('juntamsae');
+  
+    app.listen(process.env.PORT, (req, res) => {
+      console.log('listening on 8080');
+    })
+})
 
 function areYouIn205(req,res,next) {
     const ip = req.connection.remoteAddress || req.headers['x-forwarded-for'];
-    const juntamsaeIp = ['61.98.214.252','61.98.214.249']
+    const juntamsaeIp = ['61.98.214.252','61.98.214.249'];
     if(ip in juntamsaeIp) {
         next();
     }
@@ -37,13 +48,13 @@ const createSalt = async () => {
 }
 const createHashedPassword = async (pw) => {
     const salt = await createSalt();
-    const key = await pbkdf2Promise(pw, salt, process.env.CRYPTO_CODE, 64, "sha512");
+    const key = await pbkdf2Promise(pw, salt, parseInt(process.env.CRYPTO_CODE), 64, "sha512");
     const hashedPassword = key.toString("base64");
   
     return { hashedPassword, salt };
 }
 const verifyPassword = async (pw, userSalt, userPassWord) => {
-    const key = await pbkdf2Promise(pw, userSalt, process.env.CRYPTO_CODE, 64, "sha512");
+    const key = await pbkdf2Promise(pw, userSalt, parseInt(process.env.CRYPTO_CODE), 64, "sha512");
     const hashedPassword = key.toString("base64");
   
     return hashedPassword === userPassWord;
@@ -56,7 +67,7 @@ passport.use(new LocalStrategy({
     passReqToCallback: false,
 }, async (InputName, InputStudentId, done) => {
     try {
-        db.collection('login').findOne({ name: InputName }, function (err, result) {
+        db.collection('user').findOne({ name: InputName }, function (err, result) {
             if (err) return done(err)
             if (!result) return done(null, false, { message: '존재하지 않는 이름이래요' })
             verifyPassword(InputStudentId, result.salt, result.studentId).then((verified) => {
@@ -93,21 +104,11 @@ function ManagerLogined(req,res,next) {
     }
 }
 
-MongoClient.connect(process.env.DB_URL, function(err, client) {
-    if(err) return console.log(err);
-  
-    db = client.db('juntamsae');
-  
-    app.listen(process.env.PORT, function(req, res) {
-      console.log('listening on 8080');
-    })
-  })
-
 app.get('/', (req,res) => {
     res.render('main.ejs');
 })
 
-app.get('/profile/:id', (req,res) => {
+app.get('/profile/:id', Logined, (req,res) => {
     db.collection('user').findOne({_id: new ObjectId(req.params.id)}, (err,result) => {
         res.render('profile.ejs', {user: result});
     })
@@ -117,48 +118,62 @@ app.get('/login', (req,res) => {
     res.render('login.ejs');
 })
 
-app.get('/quiz/member', (req,res) => {
+app.get('/quiz/member', Logined, (req,res) => {
     db.collection('quiz').findOne({name: 'quiz'}, (err,result) => {
         res.render('quiz_member.ejs', {result: result, user: req.user});
     })
 })
 
-app.get('/quiz/manager', (req,res) => {
-    res.render('quiz_manager.ejs');
+app.get('/quiz/manager', ManagerLogined, (req,res) => {
+    db.collection('quiz').findOne({name: 'quiz'}, (err,result) => {
+        res.render('quiz_manager.ejs', {result});
+    })
 })
 
-app.get('/list', (req,res) => {
-    db.collection('user').find().toArray((err,result) => {
+app.get('/list', ManagerLogined, (req,res) => {
+    db.collection('user').find({role: 'member'}).toArray((err,result) => {
         res.render('list.ejs', {result});
     })
 })
 
-app.put('/quiz/manager/commit', (req,res) => {
+app.get('/quiz/member/modification/:id', (req,res) => {
+    db.collection('user').findOne({_id: new ObjectId(req.params.id)}, (err,user) => {
+        db.collection('quiz').findOne({name: 'quiz'}, (err,result) => {
+            res.render('quiz_member_modification.ejs', {result, user});
+        })  
+    })
+})
+
+app.put('/quiz/manager/commit', ManagerLogined, (req,res) => {
     db.collection('quiz').updateOne({name: 'quiz'}, {$set: {quiz: req.body.quiz}}, (err,result) => {
-        res.status(200).send('set quiz success');
+        res.send("<script>alert('퀴즈가 변경되었습니다!');  window.location.replace('/quiz/manager'); </script>")
     })
 })
 
-app.put('/quiz/member/answer/:id', (req,res) => {
+app.put('/quiz/member/answer/:id', Logined, (req,res) => {
     db.collection('user').updateOne({_id: new ObjectId(req.params.id)}, {$set: {answer: req.body.answer}}, (err,result) => {
-        res.status(200).send('edit quiz answer success');
+        res.send("<script>alert('퀴즈의 정답이 제출되었습니다!');  window.location.replace('/profile/" + req.params.id + "'); </script>")
     })
 })
 
-app.put('/list/answer/true/:id', (req,res) => {
+app.put('/list/answer/true/:id', ManagerLogined, (req,res) => {
     db.collection('user').updateOne({_id: new ObjectId(req.params.id)}, {$inc: {num: 1}}, (err,result) => {
         res.status(200).send('edit attendenc num +1 success');
     })
 })
 
-app.put('/list/answer/false/:id', (req,res) => {
+app.put('/list/answer/false/:id', ManagerLogined, (req,res) => {
     db.collection('user').updateOne({_id: new ObjectId(req.params.id)}, {$inc: {num: -1}}, (err,result) => {
         res.status(200).send('edit attendenc num -1 success');
     })
 })
 
 app.post('/login',passport.authenticate('local', {failureRedirect: '/fail'}) ,function(req, res) {
-    res.redirect('/quiz_member');
+    if(req.user.role === 'manager') {
+        res.redirect('/quiz/manager');
+    } else {
+        res.redirect('/quiz/member');
+    }
 });
 
 app.post('/register', (req,res) => {
@@ -167,17 +182,17 @@ app.post('/register', (req,res) => {
     createHashedPassword(req.body.studentId).then((result) => {
         createdSalt = result.salt;
         createdId = result.hashedPassword;
-        db.collection('user').find.toArray((err,result) => {
+        db.collection('user').find().toArray((err,result) => {
             result.map((a,i) => {
                 if(result.studentId == req.body.studentId) {
                     doublecheck = true;
-                    return res.send("<script>alert('이미 사용중인 학번입니다! 관리자에게 연락해주세요!');  window.location.replace('/register'); </script>");
+                    return res.send("<script>alert('이미 사용중인 학번입니다! 관리자에게 연락해주세요!');  window.location.replace('/login'); </script>");
                 }
             })
             if(!doublecheck) {
-                db.collection('user').insertOne({name: req.body.name, studentId: createdId, salt: createdSalt, num: 0, answer: ''}, (err,result) => {
-                    res.redirect('/quiz_member');
-                })
+                db.collection('user').insertOne({name: req.body.name, studentId: createdId, salt: createdSalt, num: 0, answer: '', role: 'member'}, (err,result) => {
+                    res.send("<script>alert('회원가입에 성공하였습니다! 로그인해주세요!');  window.location.replace('/login'); </script>");
+                })      
             }
         })
     })
